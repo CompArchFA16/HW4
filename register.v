aafa63ce2228a7c6190b066ab0ff345aa4584846
Bonnie Ishiguro <bgishiguro@gmail.com>
2016-10-10 22:29:17 -0400

    Deliverable 3

--- register.v
+++ register.v
@@ -31,3 +31,19 @@ input		clk
     end
 
 endmodule
+
+module register32zero
+(
+output reg[31:0]	q,
+input[31:0]			d,
+input		wrenable,
+input		clk
+);
+
+    always @(posedge clk) begin
+        if(wrenable) begin
+            q = 32'b0;
+        end
+    end
+
+endmodule
